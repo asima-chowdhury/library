1b10b8afa0bf0cad155a0a85637db81673982b93
asima <asima.chowdhury@sjinnovation.com>
2022-03-11 13:48:56 +0600

    booklist ui completed

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,51 @@
 
 import React from 'react';
+import { Button, Table } from 'react-bootstrap';
 
 const BookList = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="my-2">
+                <Button variant="dark edit">
+                    Refresh List
+                </Button>
+            </div>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Book Title</th>
+                        <th>Book Author</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>index</td>
+                        <td>title</td>
+                        <td>author</td>
+                        <td>status</td>
+                        <td>
+                            <Button
+                            variant='secondary'
+                            className='edit'
+                            
+                            >
+                                Edit
+                            </Button>
+                            <Button
+                            variant='danger'
+                            className='delete'
+                            
+                            >
+                                Delete
+                            </Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
     );
 };
 
