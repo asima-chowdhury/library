5736430d4c89776d0e67f2589bd0ad54fcdbf9eb
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-03-13 20:01:46 +0600

    read feature added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,25 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
+import BookDataService from '../services/book.services';
 
 const BookList = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        getBooks();
+    }, []);
+
+    const getBooks = async () => {
+        const data = await BookDataService.getAllBooks();
+        console.log(data.docs);
+
+        setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    };
+
     return (
         <>
+            <pre>{JSON.stringify(books, undefined, 2)}</pre>
             <div className="my-2">
                 <Button variant="dark edit">
                     Refresh List
@@ -21,28 +36,34 @@ const BookList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>index</td>
-                        <td>title</td>
-                        <td>author</td>
-                        <td>status</td>
-                        <td>
-                            <Button
-                            variant='secondary'
-                            className='edit'
-                            
-                            >
-                                Edit
-                            </Button>
-                            <Button
-                            variant='danger'
-                            className='delete'
-                            
-                            >
-                                Delete
-                            </Button>
-                        </td>
-                    </tr>
+                    {
+                        books.map((doc, index) => {
+                            return (
+                                <tr>
+                                    <td>{index+1}</td>
+                                    <td>{doc.title}</td>
+                                    <td>{doc.author}</td>
+                                    <td>{doc.status}</td>
+                                    <td>
+                                        <Button
+                                            variant='secondary'
+                                            className='edit'
+
+                                        >
+                                            Edit
+                                        </Button>
+                                        <Button
+                                            variant='danger'
+                                            className='delete'
+
+                                        >
+                                            Delete
+                                        </Button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
                 </tbody>
             </Table>
         </>
